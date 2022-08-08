63314ffbbd41687a6ea0dbb9f16e32732deee6b8
Sona <fastzeusbolt@gmail.com>
2022-08-08 04:13:38 +0300

    added reset button

--- main.js
+++ main.js
@@ -3,11 +3,25 @@ const box=document.createElement("div");
 const container=document.querySelector(".container");
 box.style.height= "10px"
 box.style.width="10px"
+box.style.backgroundColor="white"
 container.appendChild(box);
 box.classList.add("box")
+
+const reset=document.querySelector(".reset")
+reset.addEventListener("click",restart)
+
+function restart(){
+    
+   box.style.backgroundColor="white"
 }
 
+}
 const box=document.querySelectorAll(".box")
+
+
+
+
+
 
 box.forEach(box => box.addEventListener("mouseover",color))
 
